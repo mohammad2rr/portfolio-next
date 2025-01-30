7ec816a26cdc386f9a0163c8294ddd090f7e7587
mohammad2rr <mohammad2rghdd@gmail.com>
2025-01-30 23:13:38 +0330

    added redux tookit and react redux store and slice to the app2

--- src/Providers/Providers.tsx
+++ src/Providers/Providers.tsx
@@ -1,9 +1,9 @@
-"use client"; // This is necessary because PrimeReact uses client-side features
+"use client";
 
 import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/lara-light-indigo/theme.css"; // Choose a theme
-import "primereact/resources/primereact.min.css"; // Core CSS
-import "primeicons/primeicons.css"; // Icons
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "../app/store/store";
